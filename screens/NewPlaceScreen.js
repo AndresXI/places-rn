44da5a5f6922624a75bfa42b60bfcc1127988355
andres alcocer <andresalcocer7@yahoo.com>
2021-09-16 14:53:39 -0400

    implemented NewPlaceScreen component content

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,12 +1,39 @@
-import React from 'react'
-
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  TextInput,
+} from 'react-native'
+import Colors from '../constants/Colors'
 
 const NewPlaceScreen = (props) => {
+  const [title, setTitle] = useState('')
+
+  const titleChangeHandler = (text) => {
+    setTitle(text)
+  }
+
+  const savePlaceHandler = () => {}
+
   return (
-    <View>
-      <Text>New place</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          value={title}
+          onChangeText={titleChangeHandler}
+        />
+        <Button
+          title='Save Place'
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   )
 }
 
@@ -16,6 +43,16 @@ NewPlaceScreen.navigationOptions = (navDate) => {
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  form: { margin: 30 },
+  label: { fontSize: 18, marginBottom: 15 },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+})
 
 export default NewPlaceScreen
