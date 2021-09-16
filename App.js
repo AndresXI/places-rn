6b682fbfac7dd0b705c273d7d52ff09105775c12
andres alcocer <andresalcocer7@yahoo.com>
2021-09-16 09:51:41 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={styles.container}></View>
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +12,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
