e08a814e8a4b602224e611190aad56ac2fccaee4
andres alcocer <andresalcocer7@yahoo.com>
2021-09-16 14:06:23 -0400

    created navigation container

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { View, Text, StyleSheet } from 'react-native'
+
+const NewPlaceScreen = (props) => {
+  return (
+    <View>
+      <Text>New place</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default NewPlaceScreen
---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { View, Text, StyleSheet } from 'react-native'
+
+const PlacesListScreen = (props) => {
+  return (
+    <View>
+      <Text>Places list screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default PlacesListScreen
