85863203037cdc62ac3236bf8543a7a372cf4a72
andres alcocer <andresalcocer7@yahoo.com>
2021-09-18 12:17:31 -0400

    created sqlite db ini function

--- App.js
+++ App.js
@@ -5,6 +5,16 @@ import ReduxThunk from 'redux-thunk'
 
 import PlacesNavigator from './navigation/PlacesNavigator'
 import placesReducers from './store/places-reducers'
+import { init } from './helpers/db'
+
+init()
+  .then(() => {
+    console.log('initialized db')
+  })
+  .catch((err) => {
+    console.log('initialized db failed')
+    console.log(err)
+  })
 
 const rootReducer = combineReducers({
   places: placesReducers,
