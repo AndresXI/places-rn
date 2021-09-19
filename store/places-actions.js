401ebb69a0a92492b5d7d19c5e7fcf961d2de3c8
andres alcocer <andresalcocer7@yahoo.com>
2021-09-19 17:14:50 -0400

    able to store the formatted address

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,12 +1,21 @@
 import * as FileSystem from 'expo-file-system'
 
+import ENV from '../env'
 import { insertPlace, fetchPlaces } from '../helpers/db'
 
 export const ADD_PLACE = 'ADD_PLACE'
 export const SET_PLACES = 'SET_PLACES'
 
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
   return async (dispatch) => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`
+    )
+    if (!response.ok) throw 'Something went wrong'
+    const resData = await response.json()
+    if (!resData) throw 'Something went wrong'
+    const address = resData.results[0].formatted_address
+
     const fileName = image.split('/').pop()
     const newPath = FileSystem.documentDirectory + fileName
     try {
@@ -19,9 +28,9 @@ export const addPlace = (title, image) => {
       const dbResult = await insertPlace(
         title,
         newPath,
-        'Dumyy address',
-        14.2,
-        13.2
+        address,
+        location.lat,
+        location.lng
       )
       dispatch({
         type: ADD_PLACE,
@@ -29,6 +38,11 @@ export const addPlace = (title, image) => {
           id: dbResult.id,
           title,
           image: newPath,
+          address: address,
+          coords: {
+            lat: location.lat,
+            lng: location.lng,
+          },
         },
       })
     } catch (error) {
@@ -42,7 +56,6 @@ export const loadPlaces = () => {
   return async (dispatch) => {
     try {
       const dbResult = await fetchPlaces()
-      console.log('db res', dbResult)
       dispatch({ type: SET_PLACES, places: dbResult.rows._array })
     } catch (err) {
       throw err
